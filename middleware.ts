ab051bd65057ad0bfa6b083a99fb8027fbf6660a
liamdp <liam.pickering@outlook.com>
2023-04-03 22:36:27 +0100

    removed signout on images, added account disabled check to middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { withAuth } from "next-auth/middleware"
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
-        return !!token;
+        return !!token && token.userEnabled;
     },
   },
 })
